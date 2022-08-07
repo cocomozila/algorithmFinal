1b3521b44065271f5eafaac7e803e007cfa62988
cocomozila <joseyun94@naver.com>
2022-08-07 20:29:57 +0900

    No_1520

--- src/com/algorithm/No_1520.java
+++ src/com/algorithm/No_1520.java
@@ -10,8 +10,7 @@ public class No_1520 {
     static int N, M;
     static StringTokenizer st;
     static int[][] map;
-    static int[][] answer;
-    static int cnt = 0;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
 
@@ -22,122 +21,53 @@ public class No_1520 {
         N = Integer.parseInt(st.nextToken());
 
         map = new int[M][N];
-        answer = new int[M][N];
+        dp = new int[M][N];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
             }
         }
 
-        explore(0,0);
-        System.out.println(cnt);
+        int answer;
 
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(answer[i][j]+" ");
-            }
-            System.out.println();
-        }
+        answer = explore(0,0);
+        System.out.println(answer);
     }
 
-//    public static void explore(int y, int x) {
-//
-//        if (y == M-1 && x == N-1) {
-//            cnt++;
-//            return;
-//        }
-//
-//        for (int j = 0; j < M; j++) {
-//            for (int i = 0; i < N; i++) {
-//                if (i < N - 1) {
-//                    if (map[j][i] > map[j][i + 1]) {
-//                        answer[j][i]++;
-//                    }
-//                }
-//                if (j < M - 1) {
-//                    if (map[j][i] > map[j + 1][i]) {
-//                        answer[j][i]++;
-//                    }
-//                }
-//                if (j > 0) {
-//                    if (map[j][i] > map[j - 1][i]) {
-//                        answer[j][i]++;
-//                    }
-//                }
-//                if (i > 0) {
-//                    if (map[j][i] > map[j][i - 1]) {
-//                        answer[j][i]++;
-//                    }
-//                }
-//            }
-//        }
-//    }
+    public static int explore(int y, int x) {
 
-    public static void explore(int y, int x) {
+        if (y == M-1 && x == N-1) {
+            return 1;
+        }
+        if (dp[y][x] != -1) {
+            return dp[y][x];
+        }
 
-        if (y == M-1 && x == N) {
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < N; j++) {
-                    System.out.print(answer[i][j]+" ");
-                }
-                System.out.println();
-            }
-            System.exit(0);
-        }
-        if (x == N) {
-            explore(y + 1, 0);
-        }
+        dp[y][x] = 0;
 
         if (x < N - 1) {
             if (map[y][x] > map[y][x + 1]) {
-                answer[y][x]++;
+                dp[y][x] += explore(y,x + 1);
             }
         }
         if (y < M - 1) {
             if (map[y][x] > map[y + 1][x]) {
-                answer[y][x]++;
+                dp[y][x] += explore(y + 1,x);
             }
         }
         if (y > 0) {
             if (map[y][x] > map[y - 1][x]) {
-                answer[y][x]++;
+                dp[y][x] += explore(y - 1,x);
             }
         }
         if (x > 0) {
             if (map[y][x] > map[y][x - 1]) {
-                answer[y][x]++;
+                dp[y][x] += explore(y,x - 1);
             }
         }
-        explore(y, x + 1);
+        return dp[y][x];
     }
-//    public static void explore(int y, int x) {
-//
-//        if (y == M-1 && x == N-1) {
-//            cnt++;
-//            return;
-//        }
-//
-//        if (x < N - 1) {
-//            if (map[y][x] > map[y][x + 1]) {
-//                explore(y, x + 1);
-//            }
-//        }
-//        if (y < M - 1) {
-//            if (map[y][x] > map[y + 1][x]) {
-//                explore(y + 1, x);
-//            }
-//        }
-//        if (y > 0) {
-//            if (map[y][x] > map[y - 1][x]) {
-//                explore(y - 1, x);
-//            }
-//        }
-//        if (x > 0) {
-//            if (map[y][x] > map[y][x - 1]) {
-//                explore(y, x - 1);
-//            }
-//        }
-//    }
 }
