234cae551c48e70b786a76dddc0245f46e03868e
cocomozila <joseyun94@naver.com>
2022-10-21 01:24:00 +0900

    No_11399

---
+++ src/com/algorithm/No_11399.java
@@ -0,0 +1,35 @@
+package com.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class No_11399 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        int[] DP = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        DP[0] = arr[0];
+        int sum = DP[0];
+
+        for (int i = 1; i < N; i++) {
+            DP[i] = DP[i-1] + arr[i];
+            sum += DP[i];
+        }
+        System.out.println(sum);
+    }
+
+}
