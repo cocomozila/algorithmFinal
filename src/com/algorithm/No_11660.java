e459657fcc2f239504c3b39977d242435f4b29ee
cocomozila <joseyun94@naver.com>
2023-02-26 15:02:27 +0900

    No_11660

---
+++ src/com/algorithm/No_11660.java
@@ -0,0 +1,55 @@
+package com.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No_11660 {
+
+    static int N;
+    static int M;
+    static int[][] board;
+    static int[][] DP;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        board = new int[N][N];
+        DP = new int[N+1][N+1];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        fillDP();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int sum = DP[x2][y2] - (DP[x1-1][y2] + DP[x2][y1-1]) + DP[x1-1][y1-1];
+            sb.append(sum).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+    private static void fillDP() {
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 1; j < N + 1; j++) {
+                DP[i][j] = (DP[i-1][j] + DP[i][j-1]) - DP[i-1][j-1] + board[i-1][j-1];
+            }
+        }
+    }
+}
