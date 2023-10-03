2d8233c97e66990a506f7d0a3b026216ddf2bbde
cocomozila <joseyun94@naver.com>
2023-10-03 20:05:08 +0900

    No_14425 실버3 문자열 집합

---
+++ src/com/algorithm/No_14425.java
@@ -0,0 +1,35 @@
+package com.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class No_14425 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int count = 0;
+
+        Map<String, Boolean> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            map.put(br.readLine(), true);
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (map.containsKey(br.readLine())) {
+                count += 1;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
